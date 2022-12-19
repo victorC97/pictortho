42eb4b4a08b6a4670ba33999ecd1f91df7a70346
Victor Cour <v.cour@in-tracks.fr>
2022-12-19 14:21:25 +0400

    renaming

--- main.py
+++ main.py
@@ -1,55 +1,75 @@
-import pandas as pd
+import bs4.element
 import requests
 from bs4 import BeautifulSoup
+import unidecode
+from typing import Optional
+from fake_useragent import UserAgent
 import time
+import re
+import pandas as pd
 from tqdm import tqdm
-tqdm.pandas()
 
-pronoms = ["je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles", "me", "te", "se"]
-aux = ["ai", "as", "a", "avons", "avez", "ont", "avais", "avait", "avions", "aviez", "avaient", "eus", "eut", "eûmes",
-       "eûtes", "eurent", "aurais", "aurait", "aurions", "auriez", "auraient", "aurai", "auras", "aura", "aurons",
-       "aurez", "auront", "aie", "aies", "ait", "ayons", "ayez", "aient", "eusse", "eusses", "eût", "eussions",
-       "eussiez", "eussent", "suis", "es", "est", "sommes", "êtes", "sont", "étais", "était", "étions", "étiez",
-       "étaient", "fus", "fut", "fûmes", "fûtes", "furent", "serai", "seras", "sera", "serons", "serez", "seront", "serais",
-       "serait", "serions", "seriez", "seraient", "sois", "soit", "soyons", "soyez", "soient", "fusse", "fusses", "fût",
-       "fussions", "fussiez", "fussent", "ayant", "avoir", "en", "vais", "vas", "va", "allons", "allez", "vont", "viens",
-       "vient", "venons", "venez", "viennent", "étant", "être", "de", "t'es", "s'est", "t'a", "t'a", "m'étais", "t'étais", "s'était", "s'étaient", "-", "s'étant", "s'être"]
-subj = ["que", "qu'il", "qu'elle", "qu'ils", "qu'elles"]
+strip_pronoms = ["j'", "je ", "tu ", "il ", "elle ", "nous ", "vous ", "ils ", "elles "]
+strip_whitespaces = ["  "]
 
 
-def get_conjugaison(mot, dictionary):
-    res = requests.get(f"https://leconjugueur.lefigaro.fr/conjugaison/verbe/{mot}.html")
-    try:
-        if res.status_code == 200:
-            soup = BeautifulSoup(res.text.replace("<br />", " "), 'html.parser')
-            temps = soup.find_all("div", {"class": "conjugBloc"})
-            forms = []
-            for temp in temps:
-                paragraphs = temp.findAll("p")
-                if len(paragraphs) > 1:
-                    verbs = [el for el in paragraphs[1].text.replace("j'", "").replace("d'", "").split(" ") if
-                             el not in pronoms and el not in aux and el not in subj]
-                    for form in verbs:
-                        if form not in forms:
-                            forms.append(form)
-            for form in forms:
-                dictionary["Mots conjugués"].append(form)
-                dictionary["Infinitif"].append(mot)
-            return forms
-        time.sleep(3)
-    except Exception as e:
-        print(e)
+def get_verb_page(verb: str) -> Optional[BeautifulSoup]:
+    url = f"https://leconjugueur.lefigaro.fr/conjugaison/verbe/{unidecode.unidecode(verb)}.html"
+    user_agent = UserAgent()
+    headers = {"User-Agent": str(user_agent.random)}
+    q = requests.get(url, headers=headers)
+    if q.status_code == 200:
+        html_text = q.text
+        html_text = html_text.replace("<b>", "").replace("</b>", "").replace("<br />", " ")
+        soup = BeautifulSoup(html_text, parser="html.parser", features="lxml")
+        time.sleep(1)
+        return soup
+    else:
+        return None
+
+
+def find_blocs(soup: BeautifulSoup) -> list:
+    conjug_blocs = soup.find_all("div", attrs={"class": "conjugBloc"})
+    return conjug_blocs
+
+
+def get_conjug_from_bloc(bloc: bs4.element.Tag) -> list:
+    contents = bloc.contents
+    if len(contents) == 2:
+        temp = contents[0].get("id")
+        forms = contents[1].text
+        res = []
+        if temp == "temps0":
+            for pattern in strip_pronoms:
+                forms = re.sub(pattern, " ", forms, 1)
+            res = [form for form in forms.split(" ") if form != ""]
+            if len(res) == 12:
+                res = [res[i] + " " + res[i + 1] for i in range(0, len(res), 2)]
+            elif len(res) == 8:
+                res = res[:3] + [res[3] + " " + res[4], res[5] + " " + res[6], res[7]]
+        return res
+    else:
         return []
 
 
+def append_conjugaison(verb: str, conjugaisons: pd.DataFrame):
+    soup = get_verb_page(verb)
+    blocs = find_blocs(soup)
+    text = verb.split(" ")[0]
+    for bloc in blocs:
+        forms = get_conjug_from_bloc(bloc)
+        if forms != []:
+            for form in forms:
+                conjugaisons.loc[len(conjugaisons)] = [form, text]
+
+
 def main():
-    # df = pd.read_excel("table.xlsx", header=0)
-    # data_conjugaison = {"Mots conjugués": [], "Infinitif": []}
-    # df["Mots écrits"].progress_apply(lambda mot: get_conjugaison(mot, data_conjugaison))
-    # df_conjugaison = pd.DataFrame(data_conjugaison)
-    # df_conjugaison.to_csv("conjugaison.csv", sep=";")
-    pass
+    table = pd.read_excel("table.xlsx", header=0)
+    conjugaisons = pd.DataFrame(columns=["Mots conjugués", "Infinitif"])
+    table["Mots écrits"].progress_apply(lambda verb: append_conjugaison(verb, conjugaisons))
+    conjugaisons.to_csv("conjugaison.csv", sep=";", index=False)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    tqdm.pandas()
     main()
